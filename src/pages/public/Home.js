9441cd72208b035b60c838524e2241b5d449c4a9
nskhoa1502 <nskhoa1502@gmail.com>
2023-07-07 11:55:20 +0700

    no-message

--- src/pages/public/Home.js
+++ src/pages/public/Home.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Header } from "../../components";
+import * as apis from "../../apis";
 
 const Home = () => {
+  useEffect(() => {
+    const fetchDataHome = async () => {
+      try {
+        const response = await apis.getHome();
+        console.log(response.data.data);
+      } catch (error) {
+        console.error(error.response.data);
+      }
+    };
+
+    fetchDataHome();
+  }, []);
   return (
     <div className="overflow-y-auto ">
       <div className="h-[70px] px-[59px] flex items-center">
